a32590ee01f30dcb1751bb0311e9add457a4f446
Ali Rezaee <nlndipi@hotmail.com>
2023-12-09 15:30:43 +0000

    Added the new layout

--- public/config.js
+++ public/config.js
@@ -4,15 +4,26 @@ tailwind.config = {
     extend: {},
     colors: {
       gray: "#1C2938",
+      lightGray: "#DDDDDD",
       hover: "#2D3030",
       red: "#CD3F3E",
       pink: "#FF9C91",
       white: "#F4F6F6",
+      darkGreen: "#004225",
     },
     fontFamily: {
       title: ["title"],
       main: ["main"],
       comic: ["comic"],
+    },
+    container: {
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "100%",
+        "2xl": "100%",
+      },
     },
   },
   plugins: [],
